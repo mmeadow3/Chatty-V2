3d7602ca5a6c869d0269195619c5224a3bc98faa
Mike Meadows <mmeadow33@gmail.com>
2016-06-21 14:01:43 -0500

    files created and updated

---
+++ DOMhandler.js
@@ -0,0 +1,51 @@
+//////targets the button and changes the body class "light" to "dark"-defined in the CSS////////
+$("#darkTheme").click(function(){
+$(".light").toggleClass("dark")});
+///////////////////////////////////////////////////
+/////targets the text and changes it to the CSS class .largetext/////////
+$("#largeText").click(function(){
+$("#output").toggleClass("enlargeText")});
+///////////////////////////////////////////
+/////////clear button functionality//////////
+$("#clearMessages").click(function(){
+	$("#output h6").remove();   ///////removes the h6 child of the output/////////!!!! notice the spacing!!!!!!
+})
+/////////////////////////////////////////
+// $('.inputBox').keypress(function (keypress) {
+//   if (keypress.which == 13) {
+//  	//console.log("working") ////////// enter button is firing correctly
+//  	$('#output').append("<h6>" + $('.inputBox').val() + "</h6>"); /////sucessfully outputs into output box///////////
+//  	////////need input box to clear after pressing enter///////////////////////
+//  	event.preventDefault();
+//     event.currentTarget.value = ""; /////////This works, but not exactly using jQuery//////////////
+//   }
+// });
+/////////need to be able to select what user you are to add message////////////
+///will need to use :checked selector///////
+
+	
+$( ".radio input" ).click(function() {
+	// console.log("fired"); //////// Click event is working ///////
+	// console.log($('input[name=person]:checked').val()); ///////shows the person whos is being clicked
+	$('.inputBox').keypress(function (keypress) {
+  if (keypress.which == 13) {
+ 	//console.log("working") ////////// enter button is firing correctly
+ 	$('#output').append("<h6>" + $('input[name=person]:checked').val() + ": " + $('.inputBox').val() + "</h6>"); /////sucessfully outputs into output box///////////
+ 	////////need input box to clear after pressing enter///////////////////////
+ 	event.preventDefault();
+    event.currentTarget.value = ""; /////////This works, but not exactly using jQuery//////////////
+  } ////////Currently prints out more than necessary////////////
+})
+});
+
+///////Need to add delete and edit buttons//////////
+
+
+
+
+
+
+
+
+
+
